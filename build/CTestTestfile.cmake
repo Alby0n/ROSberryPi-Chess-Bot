a0a84ebac0f39dd6ae585110eb48865825175abb
Aby Stalin <73681462+Alby0n@users.noreply.github.com>
2022-06-19 22:48:10 +0530

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/chess-bot/chess-bot-v2/src
+# Build directory: /home/chess-bot/chess-bot-v2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("chessbot")
